1683858da80ca05aa9aab7c09da3ea57e8b3d381
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-03-25 09:25:53 +0300

    Added photo of Messi from Admin and updated photos.html

--- photos/urls.py
+++ photos/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    re_path(r'^$',views.artphotos,name='newsToday'),
+    re_path(r'^$',views.artphotos,name='artphotos'),
     re_path(r'^search/', views.search_results, name='search_results'),
     
 ]
